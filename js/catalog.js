f924048c202c8c72605584ceae2ae63d397acad6
Rouby-rays <88830992+Rouby-rays@users.noreply.github.com>
2021-09-03 14:23:12 +0300

    Add files via upload

---
+++ js/catalog.js
@@ -0,0 +1,11 @@
+let main = document.querySelector('.main');
+let itemList = [];
+
+for(let i = 1; i <= 8; i++) {
+  itemList.push('img/pic-' + i + '.jpg');
+};
+console.log(itemList);
+
+let newElement = document.createElement('div').classList.add;
+
+main.appendChild.newElement;
---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.burger-menu').on("click", function() {
+	console.log('erq');
+	$('.burger-menu__panel').toggleClass('active');
+  });
+	
+  
+  	
+});
+
